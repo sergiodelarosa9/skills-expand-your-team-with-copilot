ac27d3d5883909f979f9a5b4b676ed1d4762c9d5
sergiodelarosa9 <sergio_delarosa9@hotmail.com>
2026-02-13 17:53:47 -0600

    Revert "Add Manga Maniacs activity"

--- src/backend/database.py
+++ src/backend/database.py
@@ -163,17 +163,6 @@ initial_activities = {
         },
         "max_participants": 16,
         "participants": ["william@mergington.edu", "jacob@mergington.edu"]
-    },
-    "Manga Maniacs": {
-        "description": "Dive into epic adventures, powerful friendships, and unforgettable battles! Discover the captivating world of Japanese manga—from shonen action to slice-of-life stories. Share your favorite series, discuss plot twists, and connect with fellow otaku!",
-        "schedule": "Tuesdays, 7:00 PM - 8:00 PM",
-        "schedule_details": {
-            "days": ["Tuesday"],
-            "start_time": "19:00",
-            "end_time": "20:00"
-        },
-        "max_participants": 15,
-        "participants": []
     }
 }
 
